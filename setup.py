e83d9f371fa2f632bbceb1e7fd5e425b66029fa6
Kashif Sohail <kashif.ezone@gmail.com>
2026-01-10 21:17:08 +0500

    added support for python 3.12

--- setup.py
+++ setup.py
@@ -14,19 +14,21 @@ setup(name='pipelinewise-tap-postgres',
       url='https://github.com/transferwise/pipelinewise-tap-postgres',
       classifiers=[
           'License :: OSI Approved :: GNU Affero General Public License v3',
-          'Programming Language :: Python :: 3 :: Only'
+          'Programming Language :: Python :: 3 :: Only',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
       ],
-      python_requires=">=3.7,<3.10",
+      python_requires=">=3.12",
       install_requires=[
           'pipelinewise-singer-python==1.*',
-          'psycopg2-binary==2.9.5',
+          'psycopg2-binary>=2.9.5',
           'strict-rfc3339==0.7',
       ],
       extras_require={
           "test": [
-              'pytest==7.2.2',
-              'pylint==2.12.*',
-              'pytest-cov==4.0.0'
+              'pytest>=7.4.0',
+              'pylint>=3.0.0',
+              'pytest-cov>=4.1.0'
           ]
       },
       entry_points='''
